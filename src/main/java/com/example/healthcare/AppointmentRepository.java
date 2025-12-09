0b9ff3726e652c6e47bd6c81ea8e540f25a4b910
Tush248-sys <tushar.ext.work@gmail.com>
2025-12-10 02:55:00 +0530

    Delete src/main/java/com/example/healthcare directory

--- src/main/java/com/example/healthcare/AppointmentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.healthcare;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
-}
